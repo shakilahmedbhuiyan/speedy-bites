903362e0dbe0ad65a31fbe75fdfaa6b77852c81b
shakilahmedbhuiyan <16103107@iubat.edu>
2025-04-10 03:59:22 +0600

    landing hero

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,54 +1,59 @@
 <template>
-  <div class="flex flex-col w-full h-fit bg-[#374151] text-[#e5e7eb] px-16 py-14" bis_skin_checked="1">
-
+  <div
+    class="flex flex-col w-full h-fit bg-[#374151] text-[#e5e7eb] px-16 py-14"
+    bis_skin_checked="1"
+  >
     <div class="flex md:flex-row flex-col justify-center items-center" bis_skin_checked="1">
-        <div class="flex flex-col  justify-center w-2/5" bis_skin_checked="1">
-            <div class="flex items-center w-full gap-4" bis_skin_checked="1">
-                <img alt="Logo Preview" src="https://tailwind-generator.b-cdn.net/favicon.png" width="80">
-                <!---->
-                <div class="text-3xl font-bold uppercase" bis_skin_checked="1">Your Company</div>
-                <!---->
-            </div>
-            <div class="grid grid-cols-3 gap-6 mx-auto p-4" bis_skin_checked="1"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>  <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>  <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
 
-            </div>
-            <!---->
+      <div class="flex flex-col justify-center sm:w-2/5 mb-6 sb:mb-0" bis_skin_checked="1">
+        <div class="flex flex-row items-center w-full gap-3" bis_skin_checked="1">
+          <img :alt="$appName + ' logo'" src="@/assets/logo.svg" width="80" />
+          <!---->
+          <div class="capitalize" bis_skin_checked="1">
+            <p class="text-3xl font-bold font-serif">{{ $appName }}</p>
+            <p class="text-sm opacity-80">Kebab | Grill | Pizza | Burger</p>
+          </div>
+          <!---->
         </div>
+        <div class="flex flex-col text-sm" bis_skin_checked="1">
+          <p>66 London Road, Southend-On-Sea, Essex, SS1 1PG</p>
+         <div class="inline-flex items-center  gap-4">
+          <a href="tel:+4401702312675" class="hover:underline">+44 01702 312675</a>
+          <a href="mailto:hello@speedybites.uk" class="hover:underline">hello@speedybites.uk</a>
+         </div>
 
-        <div class="flex flex-row  justify-end gap-16 text-nowrap" bis_skin_checked="1">
-            <div class="grid grid-cols-3 gap-24" bis_skin_checked="1">
-                <div class="flex flex-col gap-2" bis_skin_checked="1">
-                    <div class="font-bold uppercase text-[#9ca3af] pb-3" bis_skin_checked="1">Explore</div>
-                    <a href="#xxx" class="hover:underline">Features</a>
-                     <a href="#xxx" class="hover:underline">Docs</a>
-                    <a href="#xxx" class="hover:underline">Pricing</a>
-                     <a href="#xxx" class="hover:underline">Security</a>
-
-                </div>
-                <!---->
-                <div class="flex flex-col gap-2" bis_skin_checked="1">
-                    <div class="font-bold uppercase text-[#9ca3af] pb-3" bis_skin_checked="1">Comany</div>
-                    <a href="#xxx" class="hover:underline">About Us</a>
-                     <a href="#xxx" class="hover:underline">Contact</a>
-                     <a href="#xxx" class="hover:underline">Support</a>
-                       <a href="#xxx" class="hover:underline">News</a>
-
-                </div>
-                <!---->
-                <div class="flex flex-col gap-2" bis_skin_checked="1">
-                    <div class="font-bold uppercase text-[#9ca3af] pb-3" bis_skin_checked="1">Legal</div>
-                    <a href="#xxx" class="hover:underline">Imprint</a>
-                     <a href="#xxx" class="hover:underline">Privacy Policy</a>
-                     <a href="#xxx" class="hover:underline">Terms of Use</a>
-
-                </div>
-                <!---->
-            </div>
-            <!---->
         </div>
+        <!---->
+      </div>
+
+      <div class="flex flex-row justify-end gap-16 text-nowrap" bis_skin_checked="1">
+        <div class="grid grid-cols-3 gap-24" bis_skin_checked="1">
+          <div class="flex flex-col gap-2" bis_skin_checked="1">
+            <div class="font-bold uppercase text-[#9ca3af] pb-3" bis_skin_checked="1">Explore</div>
+            <a href="#xxx" class="hover:underline">Menu</a>
+            <a href="#xxx" class="hover:underline">Opening Hours </a>
+          </div>
+          <!---->
+          <div class="flex flex-col gap-2" bis_skin_checked="1">
+            <div class="font-bold uppercase text-[#9ca3af] pb-3" bis_skin_checked="1">Comany</div>
+            <a href="#xxx" class="hover:underline">About Us</a>
+            <a href="#xxx" class="hover:underline">Contact</a>
+          </div>
+          <!---->
+          <div class="flex flex-col gap-2" bis_skin_checked="1">
+            <div class="font-bold uppercase text-[#9ca3af] pb-3" bis_skin_checked="1">Legal</div>
+            <a href="#xxx" class="hover:underline">Privacy Policy</a>
+            <a href="#xxx" class="hover:underline">Terms of Use</a>
+          </div>
+          <!---->
+        </div>
+        <!---->
+      </div>
     </div>
     <div class="w-full border-t border-gray-500 my-8" bis_skin_checked="1"></div>
-    <div class="text-center" bis_skin_checked="1">© 2025 {{ $appName }} - All rights reserved.</div>
+    <div class="text-center" bis_skin_checked="1">
+      © 2025 {{ $appName }} - All rights reserved.
+    </div>
     <!---->
-</div>
+  </div>
 </template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,13 +4,35 @@
 </script>
 
 <template>
-  <section class="max-w-4xl mx-auto flex justify-center items-center flex-col gap-4">
+  <section class="max-w-4xl mx-auto flex justify-center items-center flex-col gap-4 mb-12">
 
-    <div class="flex justify-center items-center text-center font-serif w-full my-12">
-      <h1 class="text-4xl font-bold text-center text-gray-800 dark:text-amber-100 mx-auto">
+    <div class="flex flex-col justify-center items-center text-center font-serif w-full mt-12">
+      <h1 class="text-4xl mt-8 font-bold text-center text-gray-800 dark:text-amber-100 text-shadow-2xs mx-auto">
         Kebab | Grill | Pizza | Burger
       </h1>
+      <p class="opacity-70 text-sm sm:text-lg mt-4 font-sans px-4">
+        Your one-stop destination for delicious food!<br>
+        We offer a wide variety of dishes, including kebabs, grilled items, pizzas, and burgers. <br>
+      </p>
+    </div>
 
+    <div class="w-full hero-image -skew-y-12 skew-x-5">
+      <img src="@/assets/img/2.png" alt="Welcome Image" width="350"
+      class="h-auto max-w-xl mx-auto drop-shadow-2xl motion-safe:animate-wiggle" />
+    </div>
+
+    <div>
+        <router-link to="/menu"
+        class="w-full bg-orange-500 hover:bg-amber-500 text-white font-bold py-2 px-8 rounded-full
+         transition-all duration-300 ease-in-out drop-shadow-lg inline-flex items-center hover:space-x-2 will-change-auto">
+         <span class="text-shadow"> Explore Menu</span>
+         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8"
+         fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+</svg>
+
+
+      </router-link>
     </div>
 
 
